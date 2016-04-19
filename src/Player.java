c6de9b1a4e8f1aaff4446bce29a3c887e0d805c9
gulat5020 <gulat5020@FHC-IC0026801.wrdsb.ca>
2016-04-19 09:57:05 -0400

    Player changes

--- src/Player.java
+++ src/Player.java
@@ -11,30 +11,16 @@ import javax.swing.*;
 
 public class Player {
 
-    private Image img;
-    private BufferedImage biSpriteSheet;
     int i;
     int x, y, bx, by, dx, dy, backgroundX, nYMain, nXMain, nY, nX;
     int vx, vy, nGrav, nTVelo;
     private final int SPEED = 15;
-    ImageIcon p = new ImageIcon("sprite.png");
-    int H = p.getIconHeight() / 4;
-    int W = p.getIconWidth() / 9;
     //Booleans for character movement
 //    private boolean movingLeft = false;
 //    private boolean movingRight = false;
     boolean bJump;
-
-    public void loadSprite(String sFile) {
-        try {
-            biSpriteSheet = ImageIO.read(new File("sprite.png"));;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
     int rows = 4;
     int cols = 9;
-    BufferedImage[] sprites = new BufferedImage[rows * cols];
     Rectangle r;
 
     public Player() {
@@ -51,7 +37,6 @@ public class Player {
     }
 
     public Rectangle getRect() {
-        r.setBounds(nX, nY, W, H);
         return r;
     }
 
@@ -61,9 +46,9 @@ public class Player {
         if (bJump == true && y > 276) {
             dy = -7;
         }
-           if(y <= 276){
-                bJump = false;
-            }
+        if (y <= 276) {
+            bJump = false;
+        }
         if (bJump == false) {
             dy = 7;
         }
@@ -81,12 +66,6 @@ public class Player {
         return y;
     }
 
-    public Image getImage() {
-        img = p.getImage();
-        return img;
-
-    }
-
     public void keyPressed(KeyEvent w) {
         int code = w.getKeyCode();
         if (code == KeyEvent.VK_A) {
@@ -99,7 +78,7 @@ public class Player {
         }
         if (code == KeyEvent.VK_W) {
             bJump = true;
-           
+
         }
 //        if (y <= 276) {
 //            bJump = false;
