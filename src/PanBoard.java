141a5cf116c99b335a1b57ddee12ab86477765f3
nurl3755 <nurl3755@FHC-IC0026800.wrdsb.ca>
2016-04-29 10:16:53 -0400

    Hit Detection Fixes

---
+++ src/PanBoard.java
@@ -0,0 +1,148 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+
+public class PanBoard extends JPanel implements ActionListener {
+
+    static boolean drawn = false;
+    Rectangle rB, rP;
+    //private Player p;
+    //private Enemy e;
+    Sprite p, e;
+    //private Enemy en;
+    private Timer timer;
+    private Image background;
+    static String sName;
+    Label JLabel;
+    int nChange = 1, nScroll, nScroll2, nPY, nX, nY, nDx, nDy;
+    String sFile, sFile2;
+    static int nDir;
+    BufferedImage biSpriteSheet, biSprite, biSprite2;
+    private static Background bg1, bg2;
+    static boolean bMove, bJump;
+    Rectangle P, E, G;
+
+    public PanBoard() {
+
+        sFile = "Walk (2).png";
+        sFile2 = "Sanic.png";
+        P = new Rectangle();
+        E = new Rectangle();
+        G = new Rectangle();
+        //nSpriteX = 0; // this variable is used to get the proper image from the spritesheet. I will use nDir
+        nDir = 3; // right. 0 is forward 1 is left, and 2 is back - going toward me.
+        bMove = false;
+        bJump = false;
+        nY += nDy;
+        nX += nDx;
+        nDx = 5;
+        nY = 376;
+        p = new Sprite(sFile, 350, 376, true);
+        e = new Sprite(sFile2, 200, 405, false);
+        bg1 = new Background(0, 0);
+        bg2 = new Background(765, 0);
+        addKeyListener(new Movement());
+        setFocusable(true);
+        ImageIcon i1 = new ImageIcon("Tea2.jpg");
+        background = i1.getImage();
+        timer = new Timer(30, this);
+        timer.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        p.move();
+        e.move();
+        bg1.update();
+        bg2.update();
+        P.setBounds(p.getX(), p.getY(), 64, 64);
+        E.setBounds(nX, nY, 75, 64);
+        //Ground Hit detection rectangle
+        G.setBounds(0, 438, 765, 1);
+        if (E.intersects(P)) {
+            nDx = 0;
+            System.out.println("Hit");
+
+        } else {
+            nDx = 5;
+        }
+        if (G.intersects(P)) {
+            p.y = 376;
+            bJump = true;
+            p.dy = 0;
+            //System.out.println("Hit");
+        } else {
+            bJump = false;
+        }
+        if (bMove) {
+            biSprite = p.getSprite(nDir);
+        } else {
+            biSprite = p.getStill();
+            biSprite2 = e.getStill();
+        }
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g.drawImage(background, bg1.getnScroll(), 0, this);
+        g.drawImage(background, bg2.getnScroll2(), 0, this);
+        g2d.drawImage(biSprite, p.getX(), p.getY(), null);
+        g2d.drawImage(biSprite2, nX, nY, null);
+
+
+    }
+
+    private class Movement extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            bMove = false;
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            // why split up the keyPressed function???
+            int code = e.getKeyCode();
+            if (code == KeyEvent.VK_A) {
+                nDir = 1;
+                nX += nDx;
+                bMove = true;
+            } else if (code == KeyEvent.VK_D) {
+                nDir = 3;
+                nX -= nDx;
+                bMove = true;
+            } else if (code == KeyEvent.VK_D && E.intersects(P)) {
+                nDx = 5;
+                nX -= nDx;
+                System.out.println("five");
+            }
+            if (code == KeyEvent.VK_W && bJump) {
+                p.dy = -10;
+            }
+        }
+    }
+
+    public static Background getBg1() {
+        return bg1;
+    }
+
+    public static Background getBg2() {
+        return bg2;
+    }
+
+    public static boolean bMove() {
+        return bMove;
+    }
+
+    public static int nDir() {
+        return nDir;
+    }
+}
---
+++ src/Sprite.java
@@ -0,0 +1,67 @@
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Sprite {
+
+    private BufferedImage biSpriteSheet;
+    private static final int TILE_SIZEX = 64;
+    private static final int TILE_SIZEY = 64;
+    int i;
+    int x, y, dx, dy, nDir, nGridX;
+    int nGravity = 1;
+    private final int SPEED = 15;
+    boolean bJump, isAnim;
+    int rows = 4;
+    int cols = 9;
+    Rectangle p;
+    String sFile;
+
+    public Sprite(String _sFile, int _x, int _y, boolean _isAnim) {
+        sFile = _sFile;
+        x = 350;
+        y = 376;
+        dx = 0;
+        dy = 0;
+        bJump = false;
+        nGridX = 0; // the first sprite sheet image.
+
+        try {
+            biSpriteSheet = ImageIO.read(new File(sFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void move() {
+        dy += nGravity;
+        y += dy;
+        x += dx;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public BufferedImage getSprite(int _nDir) {
+        nDir = _nDir;
+        if (nGridX == 9) {
+            nGridX = 0;
+        }
+        return biSpriteSheet.getSubimage(nGridX++ * TILE_SIZEX, nDir * TILE_SIZEY, TILE_SIZEX, TILE_SIZEY);
+    }
+
+    public BufferedImage getStill() {
+        return biSpriteSheet.getSubimage(0 * TILE_SIZEX, nDir * TILE_SIZEY, TILE_SIZEX, TILE_SIZEY);
+    }
+}
